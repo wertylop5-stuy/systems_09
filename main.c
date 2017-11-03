f1cc398a7e7fd3430038bff94b5afebf1bc1d4de
Stanley Lin <slin55055@gmail.com>
2017-11-02 21:54:25 -0400

    more formatting

--- main.c
+++ main.c
@@ -3,10 +3,12 @@
 #include"include/dir_stuff.h"
 
 int main() {
-	//printf("Size of files in '.': %lu\n", get_files_size("./"));
+
 	//printf("Size of files in '.': %s\n", better_size(get_files_size("include")));
-	printf("Size of files in '.': %s\n", better_size(my_du_abc(".")));
-	//printf("Size of files in '.': %lu\n", my_du_abc("."));
+	printf("Size of files (du -abc method) in '.': %s\n", better_size(my_du_abc(".")));
+	//printf("Size of files (du -abc method) in '.': %lu\n", my_du_abc("."));
+	
+	printf("Simple size of files in '.': %lu\n", get_files_size("."));
 	
 	printf("%s\n", list_files("./"));
 	
